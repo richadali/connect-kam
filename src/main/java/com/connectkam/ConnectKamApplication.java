c58dd812781ff39ef3f66e39b55718f6f49d63ba
richadali <richadyaminali@google.com>
2024-03-20 22:52:13 +0530

    first commit

---
+++ src/main/java/com/connectkam/ConnectKamApplication.java
@@ -0,0 +1,13 @@
+package com.connectkam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectKamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectKamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/connectkam/ConnectKamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectkam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectKamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
